e8511575f91ead3d9a4e10707452c949df36b229
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2019-12-28 17:12:31 +0100

    Fix test with invalid argument

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -12,9 +12,12 @@ fn test_command_without_arguments() -> Result<(), failure::Error> {
 #[test]
 fn test_command_with_invalid_argument() -> Result<(), failure::Error> {
     let mut cmd = Command::cargo_bin("cargo-blinc")?;
-    cmd.arg("invalid");
-    cmd.assert().failure().stderr(contains(
-        "Found argument 'invalid' which wasn't expected, or isn't valid in this context",
+    cmd.arg("--invalid");
+    cmd.assert()
+        .failure()
+        .stderr(contains("USAGE"))
+        .stderr(contains(
+        "error: Found argument '--invalid' which wasn't expected, or isn't valid in this context",
     ));
     Ok(())
 }
