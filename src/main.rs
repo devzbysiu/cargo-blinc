29c9a1ebf8a4d83f758af02f29a4565285607be8
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2020-06-30 19:06:35 +0200

    Cleanup main

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@
 #![deny(missing_docs)]
 
 use anyhow::Result;
-use args::parse_args;
-use clap::ArgMatches;
+use args::Opt;
 use config::Config;
 use log::debug;
 use std::env;
 use std::process;
+use structopt::StructOpt;
 use transition::Transition;
 
 mod args;
@@ -22,55 +22,53 @@ mod testutils;
 
 fn main() -> Result<()> {
     env_logger::init();
-    let arguments = parse_args();
-    if arguments.is_present("init") {
+    let Opt::Blinc { init, config } = Opt::from_args();
+    if init.is_some() {
         debug!("init argument passed, initializing config");
-        init_config(arguments)?;
+        // can unwrap because it's checked earlier
+        Config::default().store(init.unwrap())?;
         process::exit(0);
     }
-    let config_path = arguments
-        .value_of("config")
-        .expect("no config option passed");
-    let config = Config::get(config_path)?;
-    handle_env_variables(&config)?;
-    handle_tasks_execution(&config)?;
+    Blinc::new(Config::get(config)?)?.exec_tasks()?;
     Ok(())
 }
 
-#[allow(clippy::needless_pass_by_value)]
-fn init_config(args: ArgMatches<'_>) -> Result<()> {
-    let init_path = args
-        .value_of("init")
-        .expect("no path specified for init subcommand");
-    Config::default().store(init_path)?;
-    Ok(())
+struct Blinc {
+    config: Config,
 }
 
-fn handle_env_variables(config: &Config) -> Result<()> {
-    if let Some(env) = config.env() {
-        debug!("setting up env variables");
-        env.iter().for_each(|(k, v)| {
-            debug!("setting {} = {}", k, v);
-            env::set_var(k, v);
-        })
-    } else {
-        debug!("no env variables to set");
+impl Blinc {
+    fn new(config: Config) -> Result<Self> {
+        Blinc::init(&config)?;
+        Ok(Self { config })
     }
-    Ok(())
-}
 
-fn handle_tasks_execution(config: &Config) -> Result<()> {
-    let tx = transition(&config)?.start()?;
-    for task in config.tasks() {
-        debug!("executing {:?}", task);
-        if !task.run()?.success() {
-            tx.notify_failure()?;
-            debug!("task failed, exiting");
-            process::exit(1);
+    fn init(config: &Config) -> Result<()> {
+        if let Some(env) = config.env() {
+            debug!("setting up env variables");
+            env.iter().for_each(|(k, v)| {
+                debug!("setting {} = {}", k, v);
+                env::set_var(k, v);
+            })
+        } else {
+            debug!("no env variables to set");
         }
+        Ok(())
     }
-    tx.notify_success()?;
-    Ok(())
+
+    fn exec_tasks(&self) -> Result<()> {
+        let tx = transition(&self.config)?.start()?;
+        for task in self.config.tasks() {
+            debug!("executing {:?}", task);
+            if !task.run()?.success() {
+                tx.notify_failure()?;
+                debug!("task failed, exiting");
+                process::exit(1);
+            }
+        }
+        tx.notify_success()?;
+        Ok(())
+    }
 }
 
 fn transition(config: &Config) -> Result<Transition> {
